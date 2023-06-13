37364a8951bfd53a53a2357209be9b754cca6d3e
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-13 00:25:11 -0300

    💄 (Text) add className parameter

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,31 +1,32 @@
+import { HTMLAttributes } from 'react';
 import './styles.css';
 
-type TextProps = {
-	variant?: 'title-1' | 'title-2' | 'title-3' | 'paragraph',
-	size?: 'normal' | 'tiny' | 'small' | 'medium' | 'large' | 'largest' | 'big'
-	outlined?: 'outlined'
-};
+interface TextProps extends HTMLAttributes<HTMLElement> {
+	variant?: 'title-1' | 'title-2' | 'title-3' | 'paragraph';
+	size?: 'normal' | 'tiny' | 'small' | 'medium' | 'large' | 'largest' | 'big';
+	outlined?: 'outlined';
+}
 
-const Text = ( {variant='paragraph', size, children, outlined}: TextProps & React.PropsWithChildren): JSX.Element => {
-	const styles = `Text ${outlined === undefined? '': `Text--${outlined}`}`;
+const Text = ( {variant='paragraph', size, children, outlined, className, ...props}: React.PropsWithChildren<TextProps>): JSX.Element => {
+	const styles = `Text ${outlined === undefined? '': `Text--${outlined}`} ${className}`;
 
 	switch(variant){
 	case 'title-1':
 		return (
-			<h1 className={`${styles} Text--${size === undefined? 'big': size}`}>
+			<h1 className={`${styles} Text--${size === undefined? 'big': size}`} {...props}>
 				{children}
 			</h1>
 		);
 	case 'title-2':
 		return (
-			<h2 className={`${styles} Text--${size === undefined? 'largest': size}`}>
+			<h2 className={`${styles} Text--${size === undefined? 'largest': size}`} {...props}>
 				{children}
 			</h2>
 		);
 
 	case 'title-3':
 		return (
-			<h3 className={`${styles} Text--${size === undefined? 'large': size}`}>
+			<h3 className={`${styles} Text--${size === undefined? 'large': size}`} {...props}>
 				{children}
 			</h3>
 		);
@@ -33,7 +34,7 @@ const Text = ( {variant='paragraph', size, children, outlined}: TextProps & Reac
 	case 'paragraph':
 	default:	
 		return(
-			<p className={`${styles} Text--${size === undefined? 'normal': size}`}>
+			<p className={`${styles} Text--${size === undefined? 'normal': size}`} {...props}>
 				{children}
 			</p>
 		);
