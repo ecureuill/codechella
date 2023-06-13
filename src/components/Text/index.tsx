e9e1ceaa7ea9eae09bbf145ac26cba3d00d66278
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-12 23:52:10 -0300

    💄 (Text) add outlined modifier

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -3,28 +3,29 @@ import './styles.css';
 type TextProps = {
 	variant?: 'title-1' | 'title-2' | 'title-3' | 'paragraph',
 	size?: 'normal' | 'tiny' | 'small' | 'medium' | 'large' | 'largest' | 'big'
-
+	outlined?: 'outlined'
 };
 
-const Text = ( {variant='paragraph', size, children}: TextProps & React.PropsWithChildren): JSX.Element => {
-	
+const Text = ( {variant='paragraph', size, children, outlined}: TextProps & React.PropsWithChildren): JSX.Element => {
+	const styles = `Text ${outlined === undefined? '': `Text--${outlined}`}`;
+
 	switch(variant){
 	case 'title-1':
 		return (
-			<h1 className={`Text Text--${size === undefined? 'big': size}`}>
+			<h1 className={`${styles} Text--${size === undefined? 'big': size}`}>
 				{children}
 			</h1>
 		);
 	case 'title-2':
 		return (
-			<h2 className={`Text Text--${size === undefined? 'largest': size}`}>
+			<h2 className={`${styles} Text--${size === undefined? 'largest': size}`}>
 				{children}
 			</h2>
 		);
 
 	case 'title-3':
 		return (
-			<h3 className={`Text Text--${size === undefined? 'large': size}`}>
+			<h3 className={`${styles} Text--${size === undefined? 'large': size}`}>
 				{children}
 			</h3>
 		);
@@ -32,7 +33,7 @@ const Text = ( {variant='paragraph', size, children}: TextProps & React.PropsWit
 	case 'paragraph':
 	default:	
 		return(
-			<p className={`Text Text--${size === undefined? 'normal': size}`}>
+			<p className={`${styles} Text--${size === undefined? 'normal': size}`}>
 				{children}
 			</p>
 		);
