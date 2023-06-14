eabd4db7113c40be59c5fb428e78ee275f929d56
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-14 14:26:19 -0300

    💄 (Text) Add modifier bold 💄 (variables) remove font variables

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -5,39 +5,45 @@ interface TextProps extends HTMLAttributes<HTMLElement> {
 	variant?: 'title-1' | 'title-2' | 'title-3' | 'paragraph';
 	size?: 'normal' | 'tiny' | 'small' | 'medium' | 'large' | 'largest' | 'big';
 	outlined?: 'outlined';
+	bold?: true
 }
 
-const Text = ( {variant='paragraph', size, children, outlined, className, ...props}: React.PropsWithChildren<TextProps>): JSX.Element => {
-	const styles = `Text ${outlined === undefined? '': `Text--${outlined}`} ${className}`;
+const Text = ( {variant, size, children, outlined, className, bold, ...props}: React.PropsWithChildren<TextProps>): JSX.Element => {
+	const styles = `Text ${outlined === undefined? '': `Text--${outlined}`} ${className? className: ''}`;
 
 	switch(variant){
 	case 'title-1':
 		return (
-			<h1 className={`${styles} Text--${size === undefined? 'big': size}`} {...props}>
+			<h1 className={`${styles} Text--heading Text--${size === undefined? 'big': size}`} {...props}>
 				{children}
 			</h1>
 		);
 	case 'title-2':
 		return (
-			<h2 className={`${styles} Text--${size === undefined? 'largest': size}`} {...props}>
+			<h2 className={`${styles} Text--heading Text--${size === undefined? 'largest': size}`} {...props}>
 				{children}
 			</h2>
 		);
 
 	case 'title-3':
 		return (
-			<h3 className={`${styles} Text--${size === undefined? 'large': size}`} {...props}>
+			<h3 className={`${styles} Text--heading Text--${size === undefined? 'large': size}`} {...props}>
 				{children}
 			</h3>
 		);
 
 	case 'paragraph':
-	default:	
 		return(
-			<p className={`${styles} Text--${size === undefined? 'normal': size}`} {...props}>
+			<p className={`${styles} ${bold !== undefined ? 'Text--bold': ''} Text--${size === undefined? 'normal': size}`} {...props}>
 				{children}
 			</p>
 		);
+	default:
+		return(
+			<span className={`${styles} ${bold !== undefined ? 'Text--bold': ''} Text--${size === undefined? 'normal': size}`} {...props}>
+				{children}
+			</span>
+		);	
 	}
 };
 
